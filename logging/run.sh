abf05f76e3e4f41ad757e3cb162a22120089d294
Brendan Blanchard <brendano257@gmail.com>
2019-10-09 17:31:57 -0400

    Basic logging working, some improvements needed

--- logging/run.sh
+++ logging/run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+cd ~/FanTest/logging/data || exit
+
+# TODO: Need filename that's useful/won't overwrite, do WITHOUT cd
+
+watch -n 5 -p "date --iso-8601='seconds' >> file && sensors >> file" &
+# write date in ISO8601
+
+sleep 30;
+
+# TODO: Sleep for appropriate time
+
+kill -INT $(pidof watch)
+
+
 
