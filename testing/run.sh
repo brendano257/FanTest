27fcc9a4110600fe684153ffdd29fc56c433ebcf
Brendan Blanchard <brendano257@gmail.com>
2019-10-10 09:20:08 -0400

    Rolled running and logging into run.sh

--- testing/run.sh
+++ testing/run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+DATE=$(date +%Y_%m_%d_%H_%M);
+FILENAME="${HOME}/FanTest/log/${DATE}_run_log.txt";
+
 cd ~/FanTest/testing/GpuTest/ || exit;
 # GpuTest *must* be run from this dir or /data is inaccessible to it and it defaults to running triangle
 
-stress-ng --cpu 6 -t 30m &
-./GpuTest /test=fur /width=1920 /height=1080 /fullscreen /msaa=8 /benchmark /benchmark_duration_ms=180000 /no_scorebox /no_log_score;
+stress-ng --cpu 6 -t 1m &
+./GpuTest /test=fur /width=1920 /height=1080 /fullscreen /msaa=8 /benchmark /benchmark_duration_ms=60000 /no_scorebox /no_log_score &
+watch -n 5 -p "echo DATE::$(date --iso-8601='seconds') >> ${FILENAME} && sensors >> ${FILENAME}";
 # use the furmark test at full screen resolution, 8x anti-aliasing, for 30 minutes (180000ms), don't display or log
+
+sleep 60;
+
+kill -INT $(pidof watch);
 
 wait;
