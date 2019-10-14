9a8c593e487d67652b0777ec233473d0540b9d43
Brendan Blanchard <brendano257@gmail.com>
2019-10-14 15:38:06 -0400

    moved test cycle into runTest() and ran in sequence with 5 minute gaps before/after each run

--- testing/run.sh
+++ testing/run.sh
@@ -10,17 +10,34 @@ monitor() {
   stop=$(($1 + $(date +%s)));
 
   while [ $(date +%s) -lt $stop ]; do
-    (echo "DATE::$(date --iso-8601='seconds')" >> $FILENAME  && sensors >> $FILENAME) & sleep 2;
+    sleep 2 & (echo "DATE::$(date --iso-8601='seconds')" >> $FILENAME  && sensors >> $FILENAME) & wait;
     # write date and sensor info in background, immediately begin sleep to keep writes ~precisely on the 2s mark
   done;
+
+  # TODO: Could add annotation like "echo "STRESSEND::$(date --iso-8601='seconds'" to get exact off-time of CPU stressor
 }
 
-monitor 9000 &
-stress-ng --cpu 6 -t 10m &
-./GpuTest /test=fur /width=1920 /height=1080 /fullscreen /msaa=8 /benchmark /benchmark_duration_ms=600000 /no_scorebox /no_log_score
 
-# record time and date to file just prior to writing sensor info every 2s; totals to 900 measurements over 30min
-  # annotate date row with "DATE::" for easy processing later
-# use stress-ng's blanket CPU stressor (explicityly 6 instances (one per core) and for 30 minutes
-# use the furmark test at full screen resolution, 8x anti-aliasing, for 30 minutes (180000ms), don't display or log
+runTest() {
+  monitor 300;  # monitor without stressing for 5 minutes prior to every run
+
+  monitor 2700 &
+  stress-ng --cpu 6 -t 1800 &
+  ./GpuTest /test=fur /width=1920 /height=1080 /fullscreen /msaa=8 /benchmark /benchmark_duration_ms=1800000 /no_scorebox /no_log_score &
+  wait;
+
+  # record time and date to file just prior to writing sensor info every 2s; totals to 900 measurements over 30min
+    # annotate date row with "DATE::" for easy processing later
+  # use stress-ng's blanket CPU stressor (explicityly 6 instances (one per core) and for 30 minutes
+  # use the furmark test at full screen resolution, 8x anti-aliasing, for 30 minutes (180000ms), don't display or log
+}
+
+
+for ((i = 1; i < 4; i++)); do
+  runTest;
+done
+
+monitor 300;  # monitor without stressing for 5 minutes after last run
+
+
 
